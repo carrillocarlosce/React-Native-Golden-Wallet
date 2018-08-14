778cbe0ee00568749e7894c5786b349fb8b37dd3
carrillocarlosce <bestguru.peru@gmail.com>
2018-08-14 22:50:44 +0700

    Update UI Home Screen

--- app/modules/WalletList/elements/HomeSendButton.js
+++ app/modules/WalletList/elements/HomeSendButton.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react/native'
-import { View } from 'react-native'
+import {
+  View,
+  TouchableOpacity,
+  Image,
+  StyleSheet
+} from 'react-native'
 import PropTypes from 'prop-types'
-import ActionButton from '../../../components/elements/ActionButton'
-import constant from '../../../commons/constant'
 import images from '../../../commons/images'
-import AppStyle from '../../../commons/AppStyle'
 import MainStore from '../../../AppStores/MainStore'
+import AppStyle from '../../../commons/AppStyle'
 
 @observer
 export default class HomeSendButton extends Component {
@@ -22,18 +25,24 @@ export default class HomeSendButton extends Component {
       return <View />
     }
     return (
-      <ActionButton
-        style={{
-        }}
-        buttonItem={{
-          name: constant.SEND,
-          icon: images.iconSend,
-          background: '#121734'
-        }}
-        action={action}
-        styleText={{ color: AppStyle.mainColor, fontSize: 16 }}
-        styleIcon={{ tintColor: AppStyle.mainColor }}
-      />
+      <TouchableOpacity onPress={action}>
+        <View style={styles.btnSend}>
+          <Image
+            source={images.iconSend}
+          />
+        </View>
+      </TouchableOpacity>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  btnSend: {
+    width: 70,
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: AppStyle.backgroundDarkBlue,
+    borderRadius: 35
+  }
+})
