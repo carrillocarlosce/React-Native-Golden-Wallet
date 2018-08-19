d78f97ee27f8bf77ac8a709a86a5ccc2eee882b1
carrillocarlosce <bestguru.peru@gmail.com>
2018-08-19 12:03:07 +0700

    Prevent change state notification in setting when get error from server or no internet connection

--- app/modules/Setting/stores/AppSettingStore.js
+++ app/modules/Setting/stores/AppSettingStore.js
@@ -1,13 +1,32 @@
 import MainStore from '../../../AppStores/MainStore'
 import NotificationStore from '../../../AppStores/stores/Notification'
+import NavStore from '../../../stores/NavStore'
 
 export default class AppSettingStore {
   switchEnableNotification(isEnable) {
+    if (MainStore.appState.internetConnection === 'offline') {
+      NavStore.popupCustom.show('Network Error.')
+      return
+    }
     MainStore.appState.setEnableNotification(isEnable)
     if (isEnable) {
-      NotificationStore.addWallets()
+      NotificationStore.addWallets().then((res) => {
+        if (!res) {
+          MainStore.appState.setEnableNotification(!isEnable)
+          NavStore.popupCustom.show('Can not turn on Notification.')
+        }
+      })
     } else {
-      NotificationStore.removeWallets()
+      NotificationStore.removeWallets().then((results) => {
+        for (let i = 0; i < results.length; i++) {
+          const res = results[i]
+          if (!res.data.success && res.data.data.error !== 'not found') {
+            MainStore.appState.setEnableNotification(!isEnable)
+            NavStore.popupCustom.show('Can not turn off Notification.')
+            break
+          }
+        }
+      })
     }
   }
 }
