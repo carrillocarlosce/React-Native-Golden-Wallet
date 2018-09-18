f9046351273d1da1c9782cf99d58b76ddf8139f6
carrillocarlosce <bestguru.peru@gmail.com>
2018-09-18 11:41:23 +0700

    Add new type import and create screen

--- app/Router/CreateWalletStack.js
+++ app/Router/CreateWalletStack.js
@@ -8,6 +8,8 @@ import ScanQRCodeScreen from '../modules/ScanQRCode'
 import EnterNameViaMnemonic from '../modules/WalletImport/screen/EnterNameViaMnemonic'
 import AppStyle from '../commons/AppStyle'
 import ImportWalletScreen from '../modules/WalletImport/screen/ImportWalletScreen'
+import WalletTypeImportScreen from '../modules/WalletImport/screen/ImportTypeWalletScreen'
+import WalletTypeCreateScreen from '../modules/WalletCreate/screen/WalletTypeCreateScreen'
 
 const CreateWalletStack = StackNavigator(
   {
@@ -55,6 +57,18 @@ const CreateWalletStack = StackNavigator(
     },
     EnterNameViaMnemonic: {
       screen: EnterNameViaMnemonic,
+      navigationOptions: {
+        header: null
+      }
+    },
+    WalletTypeImportScreen: {
+      screen: WalletTypeImportScreen,
+      navigationOptions: {
+        header: null
+      }
+    },
+    WalletTypeCreateScreen: {
+      screen: WalletTypeCreateScreen,
       navigationOptions: {
         header: null
       }
