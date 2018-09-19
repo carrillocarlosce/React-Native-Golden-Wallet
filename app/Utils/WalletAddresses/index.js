e3e20ac6fe60ec678160a41117520bb6d5b04fd7
carrillocarlosce <bestguru.peru@gmail.com>
2018-09-19 16:57:04 +0700

    Added util bitcoin wallet address

--- app/Utils/WalletAddresses/index.js
+++ app/Utils/WalletAddresses/index.js
@@ -1,13 +1,17 @@
 import EthereumAddress from './Ethereum'
+import BitcoinAddress from './Bitcoin'
 
 export const chainNames = {
-  ETH: 'Ethereum'
+  ETH: 'Ethereum',
+  BTC: 'Bitcoin'
 }
 
 export default (privateKey, chainName = 'Ethereum', network = 'mainnet') => {
   switch (chainName) {
     case chainNames.ETH:
       return new EthereumAddress(privateKey)
-    default:
+    case chainNames.BTC:
+      return new BitcoinAddress(privateKey, network)
+    default: return new EthereumAddress(privateKey)
   }
 }
